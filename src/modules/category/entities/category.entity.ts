8f25ad5a9248b5a0d3584c517bdbe6513d4302b5
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-25 01:55:51 +0330

    feat(category): Create CategoryEntity for category management with hierarchical structure

---
+++ src/modules/category/entities/category.entity.ts
@@ -0,0 +1,30 @@
+import { EntityNames } from 'src/common/enum/entity.enum';
+import { BaseEntity, Column, Entity, ManyToOne } from 'typeorm';
+
+@Entity(EntityNames.Category)
+export class CategoryEntity extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  show: boolean;
+
+  @Column({ nullable: true })
+  parentId: string;
+
+  @ManyToOne(() => CategoryEntity, (category) => category.children, {
+    onDelete: 'CASCADE',
+  })
+  parent: CategoryEntity;
+
+  @ManyToOne(() => CategoryEntity, (category) => category.parent, {
+    onDelete: 'CASCADE',
+  })
+  children: CategoryEntity[];
+}
