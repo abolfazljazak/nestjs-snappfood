265607370f0348f90a57ece58ec515ab89e31175
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-24 16:00:24 +0330

    feat(swagger): Integrate Swagger documentation setup and add TypeORM configuration for PostgreSQL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { SwaggerConfigInit } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerConfigInit(app)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
