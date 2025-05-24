56f32a4f595498eca381b220d9fcd8fb4efb57f0
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-24 16:37:36 +0330

    feat(app): Integrate TypeORM module into the application module for database connectivity

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from 'src/config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig())],
 })
 export class AppModule {}
