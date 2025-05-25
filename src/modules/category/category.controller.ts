f28ddcb5a3ef97071997bba9b27384d2799ca3fe
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-25 14:11:00 +0330

    feat(category): Enhance CategoryController and DTO to support image uploads with validation

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,14 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CategoryService } from './category.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+} from "@nestjs/common";
+import { CategoryService } from "./category.service";
+import { CreateCategoryDto, UpdateCategoryDto } from "./dto/create-category.dto";
+import { ApiConsumes } from "@nestjs/swagger";
+import { SwaggerConsumes } from "src/common/enum/swagger-consumes.enum";
+import { UploadFileS3 } from "src/common/interceptors/upload-file.interceptor";
 
-@Controller('category')
+@Controller("category")
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
+  @ApiConsumes(SwaggerConsumes.MultipartData)
+  @UseInterceptors(UploadFileS3("image"))
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: "image/(png|jpg|jpeg|webp)" }),
+        ],
+      }),
+    )
+    image: Express.Multer.File,
+    @Body()
+    createCategoryDto: CreateCategoryDto,
+  ) {
     return this.categoryService.create(createCategoryDto);
   }
 
@@ -17,18 +46,21 @@ export class CategoryController {
     return this.categoryService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.categoryService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+  @Patch(":id")
+  update(
+    @Param("id") id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
     return this.categoryService.update(+id, updateCategoryDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.categoryService.remove(+id);
   }
 }
--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCategoryDto {}
+import { ApiProperty, ApiPropertyOptional, PartialType } from "@nestjs/swagger";
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  title: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  slug: string;
+
+  @ApiProperty({ format: "binary" })
+  image: string;
+
+  @ApiProperty({type: "boolean"})
+  show: boolean
+
+  @ApiPropertyOptional({ nullable: true })
+  parentId: string
+}
+
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
