bcbc26398da747b3df9ac9c32853f2233b6dbd81
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-25 12:38:07 +0330

    feat(s3): Implement S3 module and service for AWS integration, add environment variable definitions for S3 credentials

--- src/common/types/definations/env.d.ts
+++ src/common/types/definations/env.d.ts
@@ -9,5 +9,11 @@ namespace NodeJS {
     DB_HOST: string;
     DB_USERNAME: string;
     DB_PASSWORD: string;
+
+    //S3
+    S3_SECRET_KEY: string;
+    S3_ACCESS_KEY: string;
+    S3_BUKET_NAME: string;
+    S3_ENDPOINT: string;
   }
 }
