0a728a4a9b7528ecd1efeacaa82c5b632722b6c1
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-26 18:42:42 +0330

    fix(config): Enable autoLoadEntities in TypeOrm configuration for automatic entity loading

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,7 +9,7 @@ export function TypeOrmConfig(): TypeOrmModuleOptions {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    autoLoadEntities: false,
+    autoLoadEntities: true,
     synchronize: false,
     entities: [
       "dist/**/**/**/*.entity.{ts, js}",
