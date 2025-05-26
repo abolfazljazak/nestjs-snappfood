2e6654cb7149153af2ce7ece63f915fd61000acd
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-26 18:04:20 +0330

    feat(config): Add environment configuration loading from .env file and import in main application file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './config/env.config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerConfigInit } from './config/swagger.config';
