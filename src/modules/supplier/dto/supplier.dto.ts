133253f202a4acc394643e55afa9f92f757e4762
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-28 02:27:07 +0330

    feat(supplier): Implement supplier sign-up functionality with validation and conflict handling

--- src/modules/supplier/dto/supplier.dto.ts
+++ src/modules/supplier/dto/supplier.dto.ts
@@ -1,5 +1,29 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsMobilePhone, MaxLength } from "class-validator";
 
-export class CreateSupplierDto {}
+export class SupplierSignUpDto {
+  @ApiProperty()
+  @MaxLength(100)
+  manager_name: string;
 
-export class UpdateSupplierDto extends PartialType(CreateSupplierDto) {}
+  @ApiProperty()
+  @MaxLength(100)
+  manager_family: string;
+
+  @ApiProperty()
+  @IsMobilePhone("fa-IR", {}, { message: "mobile number is invalid." })
+  phone: string;
+
+  @ApiProperty()
+  @MaxLength(100)
+  store_name: string;
+
+  @ApiProperty()
+  categoryId: string;
+
+  @ApiProperty()
+  city: string;
+
+  @ApiProperty()
+  invite_code: string;
+}
--- src/modules/supplier/supplier.service.ts
+++ src/modules/supplier/supplier.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSupplierDto } from './dto/supplier.dto';
-import { UpdateSupplierDto } from './dto/update-supplier.dto';
+import { ConflictException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { SupplierEntity } from "./entities/supplier.entity";
+import { Repository } from "typeorm";
+import { SupplierSignUpDto } from "./dto/supplier.dto";
+import { CategoryService } from "../category/category.service";
 
 @Injectable()
 export class SupplierService {
-  create(createSupplierDto: CreateSupplierDto) {
-    return 'This action adds a new supplier';
-  }
+  constructor(
+    @InjectRepository(SupplierEntity)
+    private supplierRepository: Repository<SupplierEntity>,
+    private categoryService: CategoryService,
+  ) {}
 
-  findAll() {
-    return `This action returns all supplier`;
-  }
+  async signUp(signUpDto: SupplierSignUpDto) {
+    const {
+      categoryId,
+      city,
+      invite_code,
+      manager_family,
+      manager_name,
+      phone,
+      store_name,
+    } = signUpDto;
 
-  findOne(id: number) {
-    return `This action returns a #${id} supplier`;
-  }
+    const supplier = await this.supplierRepository.findOneBy({ phone });
+    if (supplier)
+      throw new ConflictException("supplier account already exist.");
+    const category = await this.categoryService.findOneById(categoryId);
+    let agent: SupplierEntity | null = null;
+    if (invite_code) {
+      agent = await this.supplierRepository.findOneBy({ invite_code });
+    }
 
-  update(id: number, updateSupplierDto: UpdateSupplierDto) {
-    return `This action updates a #${id} supplier`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} supplier`;
+    const mobileNumber = parseInt(phone);
+    const account = this.supplierRepository.create({
+      manager_family,
+      manager_name,
+      phone,
+      city,
+      store_name,
+      agentId: agent?.id,
+      categoryId: category.id,
+      invite_code: mobileNumber.toString(32).toUpperCase(),
+    });
+    await this.supplierRepository.save(account);
   }
 }
