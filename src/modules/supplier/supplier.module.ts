fe29050925b4f643a3727b5a20f1a0f6fce42ec1
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-28 02:27:00 +0330

    refactor(supplier): Remove CRUD operations from SupplierController and update SupplierModule to include TypeOrmModule and CategoryModule

--- src/modules/supplier/supplier.module.ts
+++ src/modules/supplier/supplier.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
-import { SupplierService } from './supplier.service';
-import { SupplierController } from './supplier.controller';
+import { Module } from "@nestjs/common";
+import { SupplierService } from "./supplier.service";
+import { SupplierController } from "./supplier.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SupplierEntity } from "./entities/supplier.entity";
+import { SupplierOtpEntity } from "./entities/supplier-otp.entity";
+import { CategoryModule } from "../category/category.module";
 
 @Module({
+  imports: [
+    CategoryModule,
+    TypeOrmModule.forFeature([SupplierEntity, SupplierOtpEntity]),
+  ],
   controllers: [SupplierController],
   providers: [SupplierService],
 })
